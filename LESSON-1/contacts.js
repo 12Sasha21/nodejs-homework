70ab1ae082f49237cb40f90ccbfd086485916685
12Sasha21 <90248554+12Sasha21@users.noreply.github.com>
2022-07-10 10:24:00 +0300

    one more

--- LESSON-1/contacts.js
+++ LESSON-1/contacts.js
@@ -1,1 +1,40 @@
-const contactsPath = require("./db/contacts.json");
+const fs = require("fs/promises");
+const path = require("path");
+
+const contactsPath = path.join(__dirname, "db/contacts.json");
+
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+};
+
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const result = contacts.find((item) => item.id == contactId);
+  return !result ? "contact not found" : result;
+};
+
+// function removeContact(contactId) {
+//   // ...твой код
+// }
+
+const addContact = async (name, email, phone) => {
+  const contacts = await listContacts();
+  const id = `${Number(contacts[contacts.length - 1].id) + 1}`;
+  const newContact = {
+    id: id,
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  // removeContact,
+  addContact,
+};
--- LESSON-1/index.js
+++ LESSON-1/index.js
@@ -1,4 +1,31 @@
-// const data = new Date();
+const argv = require("yargs").argv;
 
-// console.log(data.getFullYear());
-// console.log("Welcom to hell");
+const contacts = require("./contacts");
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      console.table(allContacts);
+      break;
+
+    case "get":
+      const oneContact = await contacts.getContactById(id);
+      console.log(oneContact);
+      break;
+
+    case "add":
+      const newContact = await contacts.addContact(name, email, phone);
+      console.log(newContact);
+      break;
+
+    case "remove":
+      // ... id
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction(argv);
